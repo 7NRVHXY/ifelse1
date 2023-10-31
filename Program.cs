4a8d844b302670c923e8e4a90812e121846b453e
Apk <146819953+7NRVHXY@users.noreply.github.com>
2023-10-31 20:16:06 +0400

    ilk oyrendiyim kodlar

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ifelse1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 10;
+            int filmLimitAge = 12;
+            if (age >filmLimitAge)
+            {
+                Console.WriteLine("is allowed");
+            }
+            else
+            {
+                Console.WriteLine("not allowed");
+            }
+        }
+    }
+}
